291701d17e3f6ae92fadecd8a430ccfb79675ad8
Jose Valderez <valderezp@gmail.com>
2021-12-14 11:07:51 -0300

    Project create

---
+++ src/main/java/com/valderezpe/project_delivery/ProjectDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.valderezpe.project_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valderezpe/project_delivery/ProjectDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valderezpe.project_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
